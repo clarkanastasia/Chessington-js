114ff26edd1a6d545ef80138761d49c3caa9e4f1
Anastasia C <clarkanastasia@outlook.com>
2024-01-23 10:33:31 +0000

    makes pawns move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -5,14 +5,47 @@ import Piece from './piece';
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
+        this.firstMove = true;
+    }
+
+    moveTo(board, newSquare) {
+        const currentSquare = board.findPiece(this);
+        board.movePiece(currentSquare, newSquare);
+        this.firstMove = false;
     }
 
     getAvailableMoves(board) {
-        let location = board.findPiece(this)
-        if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
+        let location = board.findPiece(this);
+        let moves = [];
+        if (this.firstMove == true){
+            if (this.player === Player.WHITE) {
+            moves = [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+         } else {
+            moves = [Square.at(location.row - 1, location.col), Square.at(location.row -2, location.col)]
+            }
         } else {
-            return Square.at(location.row - 1, location.col)
+            if (this.player === Player.WHITE) {
+                moves = [Square.at(location.row + 1, location.col)]
+             } else {
+                moves = [Square.at(location.row - 1, location.col)]
+                }
         }
+    return moves;
     }
+
+
+    // getAvailableMoves(board) {
+    //     let location = board.findPiece(this);
+    //     let moves = [];
+    //     if(this.player === Player.WHITE){
+    //         if(location.row == 1){
+    //             moves = [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+    //         } else moves = [Square.at(location.row + 1, location.col)]
+    //     } else {
+    //         if(location.row == 6){
+    //             moves = [Square.at(location.row -1, location.col), Square.at(location.row -2, location.col)]
+    //         } else moves = [Square.at(location.row -1, location.col)]
+    //     }
+    // return moves;
+    // }
 }
