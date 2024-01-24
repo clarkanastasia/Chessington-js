9a45dae723d1a732b6a51f3fa10f1f119983d0ec
Anastasia C <clarkanastasia@outlook.com>
2024-01-24 12:28:38 +0000

    makes bishop move

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../../../src/engine/square';
+
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,34 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let row = location.row;
+        let col = location.col;
+        let moves = [];
+
+// loop diagonally up and left
+        for (let i = 1; i <= Math.min(7-row, col); i++) 
+        {
+            moves.push(Square.at(row + i, col - i))
+        }
+        
+// loop diagonally up and right
+        for (let i = 1; i <= Math.min(7-row, 7-col); i++) 
+        {
+            moves.push(Square.at(row + i, col + i))
+        }
+
+// loop diagonally down and left
+        for (let i = 1; i <= Math.min(row, col); i--){
+        moves.push(Square.at(row - i, col - i))
+        }
+
+// loop diagonally down and right
+        for (let i = 1; i <= Math.min(row, 7-col); i--)
+        {
+         moves.push(Square.at(row - i, col + i))
+        }
+    
+return moves
     }
 }
