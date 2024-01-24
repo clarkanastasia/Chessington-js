396367ef0a5a636fcbe5e3b50cb46e37a8da7778
Anastasia C <clarkanastasia@outlook.com>
2024-01-24 14:40:19 +0000

    makes king and knight move

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +8,26 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let row = location.row;
+        let col = location.col;
+        let moves = [];
+
+        // declare the 8 squares
+        let front = Square.at(row + 1, col);
+        let back = Square.at(row -1 , col);
+        let left = Square.at(row, col - 1);
+        let right = Square.at(row, col +1);
+        let frontLeft = Square.at(row + 1, col - 1);
+        let frontRight = Square.at(row + 1, col +1);
+        let backLeft = Square.at(row - 1, col -1);
+        let backRight = Square.at(row -1 , col + 1);
+
+        moves = [front, back, left, right, frontLeft, frontRight, backLeft, backRight];
+        moves = moves.filter((square) => square.row <= 7 && square.row >= 0 );
+        moves = moves.filter ((square ) => square.col <= 7 && square.col >= 0 );
+
+        console.log(`Moves array: ${moves}`);
+        return moves;
     }
 }
--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +8,24 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let row = location.row;
+        let col = location.col;
+        let moves = [];
+
+        let up2Right1 = Square.at(row + 2, col + 1);
+        let up2Left1 = Square.at(row + 2, col -1);
+        let left2Up1 = Square.at(row + 1, col -2);
+        let left2Down1 = Square.at(row - 1, col -2);
+        let right2Up1 = Square.at(row + 1, col + 2);
+        let right2Down1 = Square.at(row -1, col + 2);
+        let down2Left1  = Square.at(row -2, col -1);
+        let down2Right1  = Square.at(row -2 , col + 1);
+        
+        moves = [up2Right1, up2Left1, left2Up1, left2Down1, right2Up1, right2Down1, down2Left1, down2Right1];
+        moves = moves.filter((square) => square.row <= 7 && square.row >= 0 );
+        moves = moves.filter ((square ) => square.col <= 7 && square.col >= 0 );
+        
+        return moves;
     }
 }
